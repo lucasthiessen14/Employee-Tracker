070d683a1fdf509f8e3d0e20e61399305853e045
lucasthiessen14 <66397579+lucasthiessen14@users.noreply.github.com>
2020-09-01 16:24:04 -0400

    Add files via upload

---
+++ Employee Tracker1/backend/models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//creating the user model
+//It is used to track the employee names
+const userSchema = new Schema({
+
+  //declaring usernames data type and requirment information
+  username: {type: String, require: true, unique: true, trim: true, minlength: 3},
+  address: {type: String, require: true},
+  sin_number: {type: String, require: true, unique: true}
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+//exporting the users model so it can be accessed in the other files
+module.exports = User;
